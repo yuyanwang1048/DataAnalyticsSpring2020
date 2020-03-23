044888f899c6cb303174d38469d4e99e81ccef62
yuyanwang1048 <54654161+yuyanwang1048@users.noreply.github.com>
2020-03-23 17:10:58 -0400

    Add files via upload

---
+++ Lab4.R
@@ -0,0 +1,172 @@
+# Lab
+
+# Yuyan Wang
+
+# creating a matrix data with random numbersand plotting the matrix using the image() function
+set.seed(12345)
+help(par)
+# par can be used to set or query graphical parameters.
+# Parameters can be set by specifying them as arguments 
+# to par in tag = value form, or by passing them as a list of tagged values.
+par(mar = rep(0.2,4))
+data_Matrix <-matrix(rnorm(400), nrow = 40)
+image(1:10, 1:40, t(data_Matrix)[,nrow(data_Matrix):1])
+
+par(mar=rep(0.2,4))
+heatmap(data_Matrix)
+# When we run the heatmap() here, we get the dendrograms printed on the both columns and the rows and still there is no real immerging pattern that is interesting to us, 
+help("rbinom") 
+set.seed(678910)
+for(i in 1:40){
+  # flipping a coin and getting the data
+  coin_Flip <- rbinom(1, size = 1, prob = 0.5)
+  # if the coin is "Heads", add a common pattern to that row,
+  if(coin_Flip){
+    data_Matrix[i, ] <- data_Matrix[i, ] + rep(c(0,3), each =5)
+  }
+}
+
+# Now we will plot the data
+par(mar= rep(0.2, 4))
+image(1:10, 1:40, t(data_Matrix)[, nrow(data_Matrix):1])
+# now we will run the heatmap() function on the data, we can see that, two #sets of columns are easily separated. 
+par(mar=rep(0.2, 4))
+heatmap(data_Matrix)
+
+# Lab 3
+require(rpart)
+swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(swiss_rpart) # try some different plot options
+text(swiss_rpart) # try some different text options
+
+require(party)
+
+treeSwiss<-ctree(Species ~ ., data=iris)
+plot(treeSwiss)
+
+cforest(Species ~ ., data=iris, controls=cforest_control(mtry=2, mincriterion=0))
+
+treeFert<-ctree(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+
+cforest(Fertility ~ Agriculture + Education + Catholic, data = swiss, controls=cforest_control(mtry=2, mincriterion=0))
+# look at help info, vary parameters.
+install.packages("tree")
+library(tree)
+tr <- tree(Species ~ ., data=iris)
+tr
+tr$frame
+plot(tr)
+text(tr)
+
+# Conditional Inference Tree for Mileage
+fit2M <- ctree(Mileage~Price + Country + Reliability + Type, data=na.omit(cu.summary))
+summary(fit2M)
+# plot tree
+plot(fit2M, uniform=TRUE, main="CI Tree Tree for Mileage ")
+text(fit2M, use.n=TRUE, all=TRUE, cex=.8)
+
+fitK <- ctree(Kyphosis ~ Age + Number + Start, data=kyphosis)
+plot(fitK, main="Conditional Inference Tree for Kyphosisâ€")
+plot(fitK, main="Conditional Inference Tree for Kyphosis",type="simple")
+
+# Lab 3 knn
+require(knn)
+library(class)
+data(iris)
+
+m <- dim(iris)[1]
+val <- sample(1:m, size = round(m/3), replace = FALSE, prob = rep(1/m, m)) 
+iris.learn <- iris[-val,] 	# train
+iris.valid <- iris[val,]	# test
+iris.knn <- knn(Species~., iris.learn, distance = 1, kernel = c("triangular", "epanechnikov", "biweight", "triweight", "cos", "inv", "gaussian", "rank", "optimal") )
+summary(iris.knn)
+table(predict(iris.knn,iris.valid),iris.valid$Species)
+
+head(iris.knn$W)
+head(iris.knn$D)
+head(iris.knn$C)
+head(iris.knn$fitted.values)
+
+# random forest
+require(randomForest)
+fitKF <- randomForest(Kyphosis ~ Age + Number + Start,   data=kyphosis)
+print(fitKF) 	# view results
+importance(fitKF) # importance of each predictor
+#
+fitSwiss <- randomForest(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+print(fitSwiss) # view results
+importance(fitSwiss) # importance of each predictor
+varImpPlot(fitSwiss)
+
+plot(fitSwiss)
+
+getTree(fitSwiss,1, labelVar=TRUE)
+
+help(randomForest) # look at all the package contents and the randomForest method options
+
+# look at rfcv - random forest cross-validation - 
+help(rfcv)
+
+# other data....
+data(imports85)
+
+# perform randomForest and other tree methods.....
+
+# Rpart
+require(rpart)
+swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(swiss_rpart) # try some different plot options
+text(swiss_rpart) # try some different text options
+
+# Regression Tree Example
+require(rpart)
+# build the  tree
+fitM <- rpart(Mileage~Price + Country + Reliability + Type, method="anova", data=cu.summary)
+printcp(fitM) # display the results
+plotcp(fitM)
+summary(fitM)
+par(mfrow=c(1,2)) 
+rsq.rpart(fitM) # visualize cross-validation results
+# plot tree
+plot(fitM, uniform=TRUE, main="Regression Tree for Mileage ")
+text(fitM, use.n=TRUE, all=TRUE, cex=.8)
+# prune the tree
+pfitM<- prune(fitM, cp=0.01160389) # from cptable??? adjust this to see the effect
+# plot the pruned tree
+plot(pfitM, uniform=TRUE, main="Pruned Regression Tree for Mileage")
+text(pfitM, use.n=TRUE, all=TRUE, cex=.8)
+
+# R part 3
+install.packages("mlbench")
+library(e1071)
+library(rpart)
+data(Glass, package="mlbench")
+index <- 1:nrow(Glass)
+testindex <- sample(index, trunc(length(index)/3))
+
+testset <- Glass[testindex,]
+trainset <- Glass[-testindex,]
+rpart.model <- rpart(Type ~ ., data = trainset)
+rpart.pred <- predict(rpart.model, testset[,-10], type = "class")
+printcp(rpart.model)
+plotcp(rpart.model)
+
+rsq.rpart(rpart.model)
+print(rpart.model)
+
+plot(rpart.model,compress=TRUE)
+text(rpart.model, use.n=TRUE)
+plot(rpart.pred)
+
+fitK <- rpart(Kyphosis ~ Age + Number + Start, method="class", data=kyphosis)
+printcp(fitK) # display the results
+plotcp(fitK) # visualize cross-validation results
+summary(fitK) # detailed summary of splits
+# plot tree
+plot(fitK, uniform=TRUE, main="Classification Tree for Kyphosis")
+text(fitK, use.n=TRUE, all=TRUE, cex=.8)
+# create attractive postscript plot of tree
+
+pfitK<- prune(fitK, cp=   fitK$cptable[which.min(fitK$cptable[,"xerror"]),"CP"])
+plot(pfitK, uniform=TRUE, main="Pruned Classification Tree for Kyphosis")
+text(pfitK, use.n=TRUE, all=TRUE, cex=.8)
